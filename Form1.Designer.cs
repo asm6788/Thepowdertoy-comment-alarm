c32d7761ab8b9a730ccc29012b001a674d746af9
asm6788 <asm6788@naver.com>
2016-04-23 21:21:16 +0900

    첫 커맷
    업로드

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace 파우더토이_댓글알림
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button6 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(505, 308);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "불러오기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 344);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(505, 112);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(523, 33);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(407, 426);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(638, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "모두";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(231, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(224, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(719, 1);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "댓글알림";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipTitle = "댓글알림!";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(461, 3);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(117, 21);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "동기화주기(밀리초)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(584, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "설정";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(800, 1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "취소";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(942, 458);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "파우더토이 댓글알림";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,438 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 파우더토이_댓글알림
+{
+    public partial class Form1 : Form
+    {
+        int CommentCount = 0;
+        int totalpage = 0;
+        string 제목 = null;
+        string[] 전에 = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Int()
+        {
+            HttpWebRequest wReq;
+            HttpWebResponse wRes;
+            int count = CommentCount + 20;
+            Uri uri = new Uri("http://powdertoy.co.uk/Browse/Comments.json?ID=" + textBox1.Text + "& Start =" + CommentCount + "&Count=" + count); // string 을 Uri 로 형변환
+            wReq = (HttpWebRequest)WebRequest.Create(uri); // WebRequest 객체 형성 및 HttpWebRequest 로 형변환
+            wReq.Method = "GET"; // 전송 방법 "GET" or "POST"
+            wReq.ServicePoint.Expect100Continue = false;
+            wReq.CookieContainer = new CookieContainer();
+            string res = null;
+
+            using (wRes = (HttpWebResponse)wReq.GetResponse())
+            {
+                Stream respPostStream = wRes.GetResponseStream();
+                StreamReader readerPost = new StreamReader(respPostStream, Encoding.GetEncoding("EUC-KR"), true);
+
+                res = readerPost.ReadToEnd();
+            }
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject obj = parser.Parse(res);
+            JsonArrayCollection col = (JsonArrayCollection)obj;
+
+            string[] Username = new string[22];
+            string[] CommentText = new string[22];
+            string[] Date = new string[22];
+            int i = 0;
+            foreach (JsonObjectCollection joc in col)
+            {
+                i++;
+                Username[i] = (string)joc["Username"].GetValue();
+                CommentText[i] = (string)joc["Text"].GetValue();
+                Date[i] = (string)joc["Timestamp"].GetValue();
+                Console.WriteLine(Username[i] + CommentText[i] + Date[i]);
+                TimeSpan t = TimeSpan.FromSeconds(Convert.ToInt32(Date[i]));
+                int hour = t.Hours + 9;
+                if (hour > 24)
+                {
+                    hour = hour - 24;
+                    if (hour >= 12)
+                        hour = hour + 12;
+                }
+                textBox3.AppendText("닉네임: " + Username[i] + "\r\n" + "날짜: " + hour + "시" + t.Minutes + "분" + t.Seconds + "초" + " 댓글: " + CommentText[i] + "\r\n\r\n");
+            }
+            전에 = CommentText;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int();
+            HttpWebRequest wReq;
+            HttpWebResponse wRes;
+            textBox2.Clear();
+            pictureBox1.ImageLocation = "http://static.powdertoy.co.uk/" + textBox1.Text + ".png";
+            Uri uri = new Uri("http://powdertoy.co.uk/Browse/View.json?ID=" + textBox1.Text); // string 을 Uri 로 형변환
+            wReq = (HttpWebRequest)WebRequest.Create(uri); // WebRequest 객체 형성 및 HttpWebRequest 로 형변환
+            wReq.Method = "GET"; // 전송 방법 "GET" or "POST"
+            wReq.ServicePoint.Expect100Continue = false;
+            wReq.CookieContainer = new CookieContainer();
+            string res = null;
+
+            using (wRes = (HttpWebResponse)wReq.GetResponse())
+            {
+                Stream respPostStream = wRes.GetResponseStream();
+                StreamReader readerPost = new StreamReader(respPostStream, Encoding.GetEncoding("EUC-KR"), true);
+
+                res = readerPost.ReadToEnd();
+            }
+
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject obj = parser.Parse(res);
+
+            JsonUtility.GenerateIndentedJsonText = false;
+
+            Console.WriteLine();
+            Console.WriteLine("Parsed data without indentation in JSON data format:");
+            Console.WriteLine(obj.ToString());
+            Console.WriteLine();
+
+
+            // enumerate values in json object
+            Console.WriteLine();
+            int i = 0;
+            String[] View = null;
+            View = new string[17];
+            foreach (JsonObject field in obj as JsonObjectCollection)
+            {
+                i++;
+                string name = field.Name;
+                string value = string.Empty;
+                string type = field.GetValue().GetType().Name;
+
+                // try to get value.
+                switch (type)
+                {
+                    case "String":
+                        value = (string)field.GetValue();
+                        break;
+
+                    case "Double":
+                        value = field.GetValue().ToString();
+                        break;
+
+                    case "Boolean":
+                        value = field.GetValue().ToString();
+                        break;
+
+                }
+                View[i] = value;
+                Console.WriteLine("{0} {1}",
+                    name, value);
+            }
+
+            textBox2.AppendText("요청하신 ID:" + View[1] + "\r\n");
+            textBox2.AppendText("총점수:" + View[3] + "\r\n");
+            textBox2.AppendText("보트업:" + View[4] + "\r\n");
+            textBox2.AppendText("보트다운:" + View[5] + "\r\n");
+            textBox2.AppendText("조회수:" + View[6] + "\r\n");
+            textBox2.AppendText("제목:" + View[8] + "\r\n");
+            제목 = View[8];
+            textBox2.AppendText("설명:" + View[9] + "\r\n");
+            TimeSpan t = TimeSpan.FromSeconds(Convert.ToInt32(View[11]));
+            int hour = t.Hours + 9;
+            if (hour > 24)
+            {
+                hour = hour - 24;
+                if (hour >= 12)
+                    hour = hour + 12;
+            }
+           
+            textBox2.AppendText("업로드날짜:" + hour + "시" + t.Minutes + "분" + t.Seconds + "초" + "\r\n");
+            textBox2.AppendText("제작자:" + View[12] + "\r\n");
+            textBox2.AppendText("댓글개수:" + View[13] + "\r\n");
+            if (Convert.ToInt32(View[13]) / 20 == 0)
+            {
+                totalpage = Convert.ToInt32(View[13]);
+                textBox2.AppendText("댓글 페이지 수:" + "1" + "\r\n");
+            }
+            else if (Convert.ToInt32(View[13]) / 20 == Convert.ToInt32(Convert.ToInt32(View[13]) / 20))
+            {
+                int one = Convert.ToInt32(View[13]) / 20 + 1;
+                totalpage = one;
+                textBox2.AppendText("댓글 페이지 수:" + one + "\r\n");
+            }
+            else
+            {
+                totalpage = Convert.ToInt32(View[13]) / 20;
+                textBox2.AppendText("댓글 페이지 수:" + Convert.ToInt32(View[13]) / 20 + "\r\n");
+            }
+            textBox2.AppendText("공개세이브:" + View[14] + "\r\n");
+            //if(View[16] == "")
+            //    textBox2.AppendText("태그:" + "없음" + "\r\n");
+            //else
+            //    textBox2.AppendText("태그:" + View[16] + "\r\n");
+        }
+
+        
+           
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CommentCount -= 20;
+            HttpWebRequest wReq;
+            HttpWebResponse wRes;
+            int count = CommentCount;
+            int startcount = CommentCount - 20;
+            Uri uri = new Uri("http://powdertoy.co.uk/Browse/Comments.json?ID=" + textBox1.Text + "&Start=" + startcount + "&Count=" + count); // string 을 Uri 로 형변환
+            wReq = (HttpWebRequest)WebRequest.Create(uri); // WebRequest 객체 형성 및 HttpWebRequest 로 형변환
+            wReq.Method = "GET"; // 전송 방법 "GET" or "POST"
+            wReq.ServicePoint.Expect100Continue = false;
+            wReq.CookieContainer = new CookieContainer();
+            string res = null;
+
+            using (wRes = (HttpWebResponse)wReq.GetResponse())
+            {
+                Stream respPostStream = wRes.GetResponseStream();
+                StreamReader readerPost = new StreamReader(respPostStream, Encoding.GetEncoding("EUC-KR"), true);
+
+                res = readerPost.ReadToEnd();
+            }
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject obj = parser.Parse(res);
+            JsonArrayCollection col = (JsonArrayCollection)obj;
+
+            string[] Username = new string[22];
+            string[] CommentText = new string[22];
+            string[] Date = new string[22];
+            int i = 0;
+            foreach (JsonObjectCollection joc in col)
+            {
+                i++;
+                Username[i] = (string)joc["Username"].GetValue();
+                CommentText[i] = (string)joc["Text"].GetValue();
+                Date[i] = (string)joc["Timestamp"].GetValue();
+                Console.WriteLine(Username[i] + CommentText[i] + Date[i]);
+                TimeSpan t = TimeSpan.FromSeconds(Convert.ToInt32(Date[i]));
+                int hour = t.Hours + 9;
+                if (hour > 24)
+                {
+                    hour = hour - 24;
+                    if (hour >= 12)
+                        hour = hour + 12;
+                }
+                textBox3.AppendText("닉네임: " + Username[i] + "\r\n" + "날짜: " + hour + "시" + t.Minutes + "분" + t.Seconds + "초" + " 댓글: " + CommentText[i] + "\r\n\r\n");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CommentCount += 20;
+            HttpWebRequest wReq;
+            HttpWebResponse wRes;
+            int count = CommentCount;
+            int startcount = CommentCount - 20;
+            Uri uri = new Uri("http://powdertoy.co.uk/Browse/Comments.json?ID=" + textBox1.Text + "&Start=" + startcount + "&Count=" + count); // string 을 Uri 로 형변환
+            wReq = (HttpWebRequest)WebRequest.Create(uri); // WebRequest 객체 형성 및 HttpWebRequest 로 형변환
+            wReq.Method = "GET"; // 전송 방법 "GET" or "POST"
+            wReq.ServicePoint.Expect100Continue = false;
+            wReq.CookieContainer = new CookieContainer();
+            string res = null;
+
+            using (wRes = (HttpWebResponse)wReq.GetResponse())
+            {
+                Stream respPostStream = wRes.GetResponseStream();
+                StreamReader readerPost = new StreamReader(respPostStream, Encoding.GetEncoding("EUC-KR"), true);
+
+                res = readerPost.ReadToEnd();
+            }
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject obj = parser.Parse(res);
+            JsonArrayCollection col = (JsonArrayCollection)obj;
+
+            string[] Username = new string[22];
+            string[] CommentText = new string[22];
+            string[] Date = new string[22];
+            int i = 0;
+            foreach (JsonObjectCollection joc in col)
+            {
+                i++;
+                Username[i] = (string)joc["Username"].GetValue();
+                CommentText[i] = (string)joc["Text"].GetValue();
+                Date[i] = (string)joc["Timestamp"].GetValue();
+                Console.WriteLine(Username[i] + CommentText[i] + Date[i]);
+                TimeSpan t = TimeSpan.FromSeconds(Convert.ToInt32(Date[i]));
+                int hour = t.Hours + 9;
+                if (hour > 24)
+                {
+                    hour = hour - 24;
+                    if (hour >= 12)
+                        hour = hour + 12;
+                }
+                textBox3.AppendText("닉네임: " + Username[i] + "\r\n" + "날짜: " + hour + "시" + t.Minutes + "분" + t.Seconds + "초" + " 댓글: " + CommentText[i] + "\r\n\r\n");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            progressBar1.Value = 0;
+            int i = 0;
+            string[] Username = new string[1000];
+            string[] CommentText = new string[1000];
+            string[] Date = new string[1000];
+            if (totalpage == 0)
+                MessageBox.Show("불러오기 먼저 하십시요.");
+            else
+                while (true)
+                {
+
+                    CommentCount += 20;
+                    HttpWebRequest wReq;
+                    HttpWebResponse wRes;
+                    int count = CommentCount;
+                    int startcount = CommentCount - 20;
+                    Uri uri = new Uri("http://powdertoy.co.uk/Browse/Comments.json?ID=" + textBox1.Text + "&Start=" + startcount + "&Count=" + count); // string 을 Uri 로 형변환
+                    wReq = (HttpWebRequest)WebRequest.Create(uri); // WebRequest 객체 형성 및 HttpWebRequest 로 형변환
+                    wReq.Method = "GET"; // 전송 방법 "GET" or "POST"
+                    wReq.ServicePoint.Expect100Continue = false;
+                    wReq.CookieContainer = new CookieContainer();
+                    string res = null;
+
+                    using (wRes = (HttpWebResponse)wReq.GetResponse())
+                    {
+                        Stream respPostStream = wRes.GetResponseStream();
+                        StreamReader readerPost = new StreamReader(respPostStream, Encoding.GetEncoding("EUC-KR"), true);
+
+                        res = readerPost.ReadToEnd();
+                    }
+                    JsonTextParser parser = new JsonTextParser();
+                    JsonObject obj = parser.Parse(res);
+                    JsonArrayCollection col = (JsonArrayCollection)obj;
+
+
+
+                    foreach (JsonObjectCollection joc in col)
+                    {
+                        i++;
+
+                        Console.WriteLine((double)i / (double)totalpage);
+                        progressBar1.Value = Convert.ToInt32((double)i / (double)totalpage * 100);
+                        Username[i] = (string)joc["Username"].GetValue();
+                        CommentText[i] = (string)joc["Text"].GetValue();
+                        Date[i] = (string)joc["Timestamp"].GetValue();
+                        Console.WriteLine(Username[i] + CommentText[i] + Date[i]);
+                        TimeSpan t = TimeSpan.FromSeconds(Convert.ToInt32(Date[i]));
+                        int hour = t.Hours + 9;
+                        if (hour > 24)
+                        {
+                            hour = hour - 24;
+                            if (hour >= 12)
+                                hour = hour + 12;
+                        }
+                        if (i == totalpage)
+                        {
+                            goto Out;
+                        }
+                        textBox3.AppendText("닉네임: " + Username[i] + "\r\n" + "날짜: " + hour + "시" + t.Minutes + "분" + t.Seconds + "초" + " 댓글: " + CommentText[i] + "\r\n\r\n");
+                    }
+                }
+            Out:;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            if (totalpage == 0)
+                MessageBox.Show("불러오기 먼저 하십시요.");
+            else
+            {
+                MessageBox.Show("설정완료");
+                timer1.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(textBox4.Text);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            HttpWebRequest wReq;
+            HttpWebResponse wRes;
+            int count = CommentCount + 20;
+            Uri uri = new Uri("http://powdertoy.co.uk/Browse/Comments.json?ID=" + textBox1.Text + "& Start =" + CommentCount + "&Count=" + count); // string 을 Uri 로 형변환
+            wReq = (HttpWebRequest)WebRequest.Create(uri); // WebRequest 객체 형성 및 HttpWebRequest 로 형변환
+            wReq.Method = "GET"; // 전송 방법 "GET" or "POST"
+            wReq.ServicePoint.Expect100Continue = false;
+            wReq.CookieContainer = new CookieContainer();
+            string res = null;
+
+            using (wRes = (HttpWebResponse)wReq.GetResponse())
+            {
+                Stream respPostStream = wRes.GetResponseStream();
+                StreamReader readerPost = new StreamReader(respPostStream, Encoding.GetEncoding("EUC-KR"), true);
+
+                res = readerPost.ReadToEnd();
+            }
+            JsonTextParser parser = new JsonTextParser();
+            JsonObject obj = parser.Parse(res);
+            JsonArrayCollection col = (JsonArrayCollection)obj;
+
+            string[] Username = new string[22];
+            string[] CommentText = new string[22];
+            string[] Date = new string[22];
+            int i = 0;
+            foreach (JsonObjectCollection joc in col)
+            {
+                i++;
+                Username[i] = (string)joc["Username"].GetValue();
+                CommentText[i] = (string)joc["Text"].GetValue();
+                Date[i] = (string)joc["Timestamp"].GetValue();
+                Console.WriteLine(Username[i] + CommentText[i] + Date[i]);
+                TimeSpan t = TimeSpan.FromSeconds(Convert.ToInt32(Date[i]));
+                int hour = t.Hours + 9;
+                if (hour > 24)
+                {
+                    hour = hour - 24;
+                    if (hour >= 12)
+                        hour = hour + 12;
+                }
+                textBox3.AppendText("닉네임: " + Username[i] + "\r\n" + "날짜: " + hour + "시" + t.Minutes + "분" + t.Seconds + "초" + " 댓글: " + CommentText[i] + "\r\n\r\n");
+            }
+
+            if (전에[1] != CommentText[1])
+            {              
+                notifyIcon1.Visible = true; // 트레이의 아이콘을 보이게 한다.
+                notifyIcon1.BalloonTipText = CommentText[1];
+                notifyIcon1.ShowBalloonTip(500);
+                전에 = CommentText;
+                timer1.Start();
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("댓글알림 정지");
+        }
+    }
+}
